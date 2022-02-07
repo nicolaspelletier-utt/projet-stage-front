73242e1c3c16a44cd9c4ef257eaec6a7276f6dff
Nicolas Pelletier <nicolas@grytics.com>
2022-02-07 12:05:08 +0100

    add routing

---
+++ src/pages/Table.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class Table extends React.Component {
+    render() {
+        return <h1>Page Table</h1>
+    }
+}
+export default Table;
---
+++ src/route.js
@@ -0,0 +1,22 @@
+const routes = [
+    {
+        'name': 'login',
+        'path': '/login',
+        'component': Login,
+    },
+    {
+        'name':'home',
+        'path': '/',
+        'component': App,
+    },
+    {
+        'name': 'table',
+        'path': '/table',
+        'component': Table,
+    },
+    {
+        'name': 'stats',
+        'path': '/stats',
+        'component': Stats,
+    },
+];
