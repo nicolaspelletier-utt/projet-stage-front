912468e259278fd8185c5fea296f43e68621dc2c
Nicolas Pelletier <nicolas@grytics.com>
2022-02-10 16:12:00 +0100

    update Stats.js & Home.js use hooks

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,53 +1,15 @@
 import React from 'react';
 import Navbar from '../components/navbar';
 import { Navigate } from 'react-router-dom';
-
-class Home extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLogged: false,
-            isLoaded: false
-        };
-    }
-    componentDidMount() {
-        
-        let result=fetch("http://localhost:9090/api/islogged", {
-          credentials: 'include'
-    
-        })
-        .then(res => res.json())
-        .then((result) => {
-          console.log(result);
-          if (!result.logged) {
-            this.setState({isLogged: false});
-          }
-          else {
-            this.setState({isLogged: true});
-          }
-          this.setState({isLoaded: true});
-
-          
-        });
-      }
-    render() {
-        const {isLogged, isLoaded} = this.state;
-        if (isLogged) {
-            return <div>
-                    <Navbar/>
-                       <div className='container'>
-                           <br/><br/>
-                       <h1 className='display-6'>Bienvenue sur le projet préparatoire de Nicolas</h1>
-                       </div> 
-            </div>
-        }
-        else if (!isLoaded && !isLogged) {
-            return <div>Chargement . . .</div>
-        }
-        else {
-            return <Navigate to="/login" />
-        }
-
-    }
+function Home() {
+  return (
+    <div>
+      <Navbar/>
+      <div className='container'>
+      <br/><br/>
+      <h1 className='display-6'>Bienvenue sur le projet préparatoire de Nicolas</h1>
+      </div> 
+    </div>
+  );
 }
 export default Home;
--- src/pages/Stats.js
+++ src/pages/Stats.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../components/navbar.jsx';
 import { Navigate } from 'react-router-dom';
 
 import StatsComments from '../components/statsComments.jsx';
 import StatsUsers from '../components/statsUsers.jsx';
 import StatsNoInterraction from '../components/statsNoInterraction.jsx';
+function Stats() {
+    const [begin,setBegin] = useState("");
+    const [end,setEnd] = useState("");
+    const [beginTmp,setBeginTmp] = useState("");
+    const [endTmp,setEndTmp] = useState("");
+    return (
+        <div>
+            <Navbar/>
+            <div className='container'>
+                <br/><br/>
+            <h1>Page Stats</h1><br/><br/>
+            <h4>Choix de la période : </h4>
+            <form className='form-group'>
+            <input type="date" id="begin" className="form-control" name="begin" onChange={(e) => setBegin(e.target.value)}></input>
+            <br/>
+            <input type="date" id="end" className="form-control" name="end" onChange={(e) => setEnd(e.target.value)}></input>
+            </form>
+            <div className='container'>
+            <h4>Nombre moyen de commentaire : </h4>
+            <div className='container'><br/>
+            <div className='font-weight-bold'><StatsComments begin={begin} end={end}/></div>
+            </div>
+            </div>
+            <br/><br/>
+            <div className='container'>
+                <h4>Top 10 des utiliseurs ayant le plus interragis</h4><br/>
+            <StatsUsers begin={begin} end={end}/>
+            </div>
+            <div className='container'>
+                <h4>Top 10 des utiliseurs n'ayant pas interragis</h4><br/>
+            <StatsNoInterraction begin={begin} end={end}/>
+            </div>
+            <br/><br/>
+            </div>
+        </div>
+    );
+}
+export default Stats;
+/*
 class Stats extends React.Component {
     constructor(props) {
         super(props);
@@ -53,4 +92,4 @@ class Stats extends React.Component {
        
     }
 }
-export default Stats;
+export default Stats;*/
