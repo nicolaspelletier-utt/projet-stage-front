cb9d283b36a155dff1b04b01d027bd660d7424b7
Nicolas Pelletier <nicolas@grytics.com>
2022-02-10 17:19:47 +0100

    update statsUsers with hooks and date selection

--- src/components/statsUsers.jsx
+++ src/components/statsUsers.jsx
@@ -1,5 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+function StatsUsers(props) {
+    const [error,setError] = useState("");
+    const [isLoaded,setIsLoaded] = useState(false);
+    const [value,setValue] = useState([]);
+    const begin=props.begin;
+    const end=props.end;
+    
 
+    useEffect(() => {
+        console.log(props);
+        fetch(`http://localhost:9090/api/stats/users?begin=${begin}&end=${end}`,{
+            method: "GET",
+            credentials: "include"
+        },)
+        .then(res => res.json())
+        .then((response) => {
+            setIsLoaded(true);
+            setValue(response)
+        },(error) => {
+            setError(error);
+            setIsLoaded(true);
+        });
+    },[begin,end])
+    const values = value.map(function(table) {
+        return (
+            <tr>
+                <td scope="row">{table.id}</td>
+                <td scope="row">{table.name}</td>
+                <td scope="row">{table.count}</td>
+            </tr>);
+    })
+    if (!isLoaded) {
+        return (<div>Chargement . . .</div>);
+    }
+    else if (isLoaded && error) {
+        return (<div>Erreur : {error}</div>);
+    }
+    else {
+        return (<div>
+            <div>
+                  <table className='table'>
+                      <thead>
+                          <tr>
+                              <th scope="col">#</th>
+                              <th scope="col">Name</th>
+                              <th scope="col">Count</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                        {values}
+                      </tbody>
+
+                  </table>
+              </div>
+        </div>)
+    }
+} 
+export default StatsUsers;
+
+/*
 class StatsUsers extends React.Component {
     constructor(props) {
         super(props);
@@ -63,4 +122,4 @@ class StatsUsers extends React.Component {
           }
     }
 }
-export default StatsUsers;
+export default StatsUsers;*/
