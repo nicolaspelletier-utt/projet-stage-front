3553943f5382455057a9457772bf99bcbc92f252
Nicolas Pelletier <nicolas@grytics.com>
2022-02-10 15:51:43 +0100

    update Table.js use hooks and not class

--- src/pages/Table.js
+++ src/pages/Table.js
@@ -1,24 +1,13 @@
-import React from 'react';
+import React , {useState, useEffect} from 'react';
 import Navbar from '../components/navbar';
 import { Navigate } from 'react-router-dom';
-class Table extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+function Table() {
 
-        };
-    }
-   
-    render() {
-        const {isLogged,isLoaded} = this.state;
-     
-            return <div>
+    return (
+        <div>
             <Navbar/>
-            <h1>Page Table</h1>
-    
+            <h1>Page table</h1>
         </div>
-
-
-    }
+    );
 }
 export default Table;
