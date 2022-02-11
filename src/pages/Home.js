6f5cdc56f354614e313bc8db5415306824072ecd
Nicolas Pelletier <nicolas@grytics.com>
2022-02-11 13:49:02 +0100

    add auth check in homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Navbar from '../components/navbar';
 import { Navigate } from 'react-router-dom';
+import IsLogged from '../components/isLogged.jsx';
 function Home() {
+  
   return (
     <div>
+      <IsLogged/>
       <Navbar/>
       <div className='container'>
       <br/><br/>
