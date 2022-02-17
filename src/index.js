5423c7cc1f13f72ad586880d041175f3361b897a
Nicolas Pelletier <nicolas@grytics.com>
2022-02-17 17:21:54 +0100

    remove useless code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-//import reportWebVitals from './reportWebVitals';
-//import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './pages/Login.js';
 import Home from './pages/Home.js';
@@ -25,15 +22,3 @@ export default function App() {
   );
 }
 ReactDOM.render(<App />, document.getElementById("root"));
-/*
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();*/
